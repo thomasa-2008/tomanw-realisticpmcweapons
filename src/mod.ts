093edae608b54ad596fe433d7147f5a8bdbfa5f0
Thomas A <162984336+thomasa-2008@users.noreply.github.com>
2024-07-11 22:38:20 +1000

    V2.0.0
    usec:
    scar-h
    mp5
    mp9 x2
    aug a1
    aug a3
    mdr 556
    g36
    sa58
    m700
    g28
    m1a
    mk18
    rsass
    m3 super 90
    m590a1
    m870
    p90
    vec45
    vec9mm
    ump45
    mk47
    rfb
    bear:
    vss
    pkm/pkp
    ks23m
    mp shot
    saiga12
    svds
    t5000
    dvl10
    rd704
    ash12
    ak104
    ak105
    vsk
    9a91

--- src/mod.ts
+++ src/mod.ts
@@ -6,26 +6,26 @@ import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
 import { ItemHelper } from "@spt/helpers/ItemHelper";
 import { BaseClasses } from "@spt/models/enums/BaseClasses";
 
-class M4A1_PMC implements IPostDBLoadMod
+class REALISTIC_PMC_WEP implements IPostDBLoadMod
 {
 
     private mod_config = require("../config/config.json");
     public postDBLoad(container: DependencyContainer): void
     {
         // get database from server
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-        const tables: IDatabaseTables = databaseServer.getTables();
+        const dbServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const db: IDatabaseTables = dbServer.getTables();
 
 
         // usec gear
-        tables.bots.types["usec"].inventory.equipment.FirstPrimaryWeapon = this.mod_config.usec_primary;
-        tables.bots.types["usec"].inventory.equipment.Holster = this.mod_config.usec_pistol;
+        db.bots.types["usec"].inventory.equipment.FirstPrimaryWeapon = this.mod_config.usec_primary;
+        db.bots.types["usec"].inventory.equipment.Holster = this.mod_config.usec_pistol;
         
         // bear gear
-        tables.bots.types["bear"].inventory.equipment.FirstPrimaryWeapon = this.mod_config.bear_primary;
-        tables.bots.types["bear"].inventory.equipment.Holster = this.mod_config.bear_pistol;
+        db.bots.types["bear"].inventory.equipment.FirstPrimaryWeapon = this.mod_config.bear_primary;
+        db.bots.types["bear"].inventory.equipment.Holster = this.mod_config.bear_pistol;
 
     }
 }
 
-export const mod = new M4A1_PMC();
+export const mod = new REALISTIC_PMC_WEP();
